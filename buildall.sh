68eb027fc5dcb7dc4134e5f512789ff393406693
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2021-08-21 23:59:33 +0900

    update

--- buildall.sh
+++ buildall.sh
@@ -1,11 +1,21 @@
 PRJDIR=$(dirname $0)
 
-west -v build -d build_m33_hard_lwchar -b nrf5340dk_nrf5340_cpuapp $PRJDIR -- -DCONF_FILE='prj.conf' 
-west -v build -d build_m33_hard_swchar -b nrf5340dk_nrf5340_cpuapp $PRJDIR -- -DCONF_FILE='prj.conf                         overlay-fshort-wchar.conf'
-west -v build -d build_m33_soft_lwchar -b nrf5340dk_nrf5340_cpuapp $PRJDIR -- -DCONF_FILE='prj.conf overlay-soft-float.conf' 
-west -v build -d build_m33_soft_swchar -b nrf5340dk_nrf5340_cpuapp $PRJDIR -- -DCONF_FILE='prj.conf overlay-soft-float.conf overlay-fshort-wchar.conf' 
-west -v build -d build_m4_hard_lwchar  -b nrf52840dk_nrf52840      $PRJDIR -- -DCONF_FILE='prj.conf' 
-west -v build -d build_m4_hard_swchar  -b nrf52840dk_nrf52840      $PRJDIR -- -DCONF_FILE='prj.conf                         overlay-fshort-wchar.conf' 
-west -v build -d build_m4_soft_lwchar  -b nrf52840dk_nrf52840      $PRJDIR -- -DCONF_FILE='prj.conf overlay-soft-float.conf' 
-west -v build -d build_m4_soft_swchar  -b nrf52840dk_nrf52840      $PRJDIR -- -DCONF_FILE='prj.conf overlay-soft-float.conf overlay-fshort-wchar.conf' 
+declare -A cores=(
+                   ["cortex-m4"]='nrf52840dk_nrf52840'
+                   ["cortex-m33"]='nrf5340dk_nrf5340_cpuapp'
+)
+declare -A abis=(
+                   ["hard-float"]='prj.conf overlay-hard-float.conf'
+       ["hard-float/short-wchar"]='prj.conf overlay-soft-float.conf overlay-fshort-wchar.conf'
+                   ["soft-float"]='prj.conf overlay-hard-float.conf'
+       ["soft-float/short-wchar"]='prj.conf overlay-soft-float.conf overlay-fshort-wchar.conf'
+)
 
+for C in "${!cores[@]}" ; do
+	for A in "${!abis[@]}" ; do 
+		mkdir -p build/${C}/${A}
+		mkdir -p lib/${C}/${A}
+		west -v build -d build/${C}/${A} -b ${cores[${C}]} $PRJDIR -- -DCONF_FILE="${abis[${A}]}"
+		cp `find build/${C}/${A}/modules/nrfxlib/nrfxlib/ -name '*.a'` lib/${C}/${A}/
+	done
+done
